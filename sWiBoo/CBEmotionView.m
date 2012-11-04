c3a14dc4a4d975255eb2523c944a8ac7f606cd7c
ly <6david9@163.com>
2012-11-04 16:22:16 +0800

    添加autoreleasepool，释放图片内存

--- sWiBoo/CBEmotionView.m
+++ sWiBoo/CBEmotionView.m
@@ -36,29 +36,30 @@
     NSUInteger rowPosition = 0;
     
     for (NSUInteger i = 0; i < 146; i++) {
-        
-        columPosition = i % numPerLine;
-        
-        if (columPosition == 0 && i > columPosition)
-            rowPosition += 1;
-        
-        NSString *emotionName = [NSString stringWithFormat:@"%03d.gif", i+1];
-        NSData *emotionData = [NSData dataWithContentsOfFile:[[[NSBundle mainBundle] resourcePath]stringByAppendingPathComponent:emotionName]];
-        
-        CGFloat x = columPosition * kEmotionWidth;
-        CGFloat y = rowPosition * kEmotionHeight;
-        CGRect gifFrame = CGRectMake(x, y, 35, 35);
-        UIWebView *gifShower = [[UIWebView alloc] initWithFrame:gifFrame];
-        
-        [gifShower.scrollView setMinimumZoomScale:0.1];
-        if ((i+1) <=104)
-            [gifShower.scrollView setZoomScale:0.58];
-        
-        gifShower.userInteractionEnabled = NO;
-        gifShower.scrollView.contentSize = CGSizeMake(35, 35);
-        [gifShower loadData:emotionData MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
-        
-        [self addSubview:gifShower];
+        @autoreleasepool {
+            columPosition = i % numPerLine;
+            
+            if (columPosition == 0 && i > columPosition)
+                rowPosition += 1;
+            
+            NSString *emotionName = [NSString stringWithFormat:@"%03d.gif", i+1];
+            NSData *emotionData = [NSData dataWithContentsOfFile:[[[NSBundle mainBundle] resourcePath]stringByAppendingPathComponent:emotionName]];
+            
+            CGFloat x = columPosition * kEmotionWidth;
+            CGFloat y = rowPosition * kEmotionHeight;
+            CGRect gifFrame = CGRectMake(x, y, 35, 35);
+            UIWebView *gifShower = [[UIWebView alloc] initWithFrame:gifFrame];
+            
+            [gifShower.scrollView setMinimumZoomScale:0.1];
+            if ((i+1) <=104)
+                [gifShower.scrollView setZoomScale:0.58];
+            
+            gifShower.userInteractionEnabled = NO;
+            gifShower.scrollView.contentSize = CGSizeMake(35, 35);
+            [gifShower loadData:emotionData MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
+            
+            [self addSubview:gifShower];
+        }
     }
 
 }
