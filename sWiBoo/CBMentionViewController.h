0216a9f0430f8a684493ae4cc969224c14f0c0b3
ly <6david9@163.com>
2013-02-20 18:32:08 +0800

    添加mention好友功能

---
+++ sWiBoo/CBMentionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBMentionViewController.h
+//  sWiBoo
+//
+//  Created by ly on 13-2-20.
+//  Copyright (c) 2013年 Lei Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBMentionViewController : UIViewController
+
+@end
---
+++ sWiBoo/CBMentionViewController.m
@@ -0,0 +1,38 @@
+//
+//  CBMentionViewController.m
+//  sWiBoo
+//
+//  Created by ly on 13-2-20.
+//  Copyright (c) 2013年 Lei Yan. All rights reserved.
+//
+
+#import "CBMentionViewController.h"
+
+@interface CBMentionViewController ()
+
+@end
+
+@implementation CBMentionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
